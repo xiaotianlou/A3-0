4397ff6c51ac22c0d9463e0cfc4293a90ff6fc65
ljm-jim <ljm-jim@hotmail.com>
2023-03-26 18:54:21 -0400

    Whittaker Diagrams

--- island/src/main/java/featureRenderer/WhittakerDiagramsRenderer.java
+++ island/src/main/java/featureRenderer/WhittakerDiagramsRenderer.java
@@ -1,4 +1,13 @@
 package featureRenderer;
 
+import transformation.builtinADT.MeshADT;
+import transformation.builtinADT.PolygonADT;
+
 public class WhittakerDiagramsRenderer {
+    public MeshADT WhittakerDiagramGenerator(MeshADT mesh){
+        for (PolygonADT p: mesh.getPolygons()){
+
+        }
+        return mesh;
+    }
 }
