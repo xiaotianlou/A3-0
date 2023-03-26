3980aae50dbbae7365cb904664e4becc6e92ab86
ljm-jim <ljm-jim@hotmail.com>
2023-03-26 18:39:04 -0400

    Whittaker Diagrams

---
+++ island/src/main/java/featureRenderer/WhittakerDiagramsRenderer.java
@@ -0,0 +1,4 @@
+package featureRenderer;
+
+public class WhittakerDiagramsRenderer {
+}
--- island/src/main/java/transformation/builtinADT/Biome.java
+++ island/src/main/java/transformation/builtinADT/Biome.java
@@ -2,6 +2,4 @@ package transformation.builtinADT;
 
 public enum Biome {
     Tropical_Rain_Forest, Tropical_Seasonal_Forest, Temperate_Rain_Forest, Temperate_Deciduous_Forest, None, Temperate_Grassland_and_Desert, Taiga, Tundra, Subtropical_Desert
-
-
 }
